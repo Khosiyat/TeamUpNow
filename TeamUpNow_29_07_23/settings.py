320d3d54c0c16dd2e9e1790d6e586d631ca7f0a5
Khosiyat Sabirova <hosiyatsabirova@gmail.com>
2023-11-14 15:50:18 +0100

    Update settings.py

--- TeamUpNow_29_07_23/settings.py
+++ TeamUpNow_29_07_23/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%ly_iova&p8lo9_c#e)15d60ui!q_5@t2iypm=%3ki=04*4$qy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['16.171.242.46', 'www.teamupnow.tech', 'teamupnow.tech']
+ALLOWED_HOSTS = ['16.171.242.46', 'www.teamupnow.tech', 'teamupnow.tech', 'ip-172-31-30-212.eu-north-1.compute.internal']
 
 
 # Application definition
